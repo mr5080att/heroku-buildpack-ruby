5c70cbf1e6ffb611652d03f5fe93de3b82538860
Michael Reyes <mr5080@att.com>
2013-10-24 12:05:25 -0700

    swap command to run

--- lib/language_pack/backup.rb
+++ lib/language_pack/backup.rb
@@ -11,20 +11,20 @@ class LanguagePack::Backup < LanguagePack::Rails4
   end
 
   def compile
+    super
     install_postgresql_client
     install_backup_gem
-    super
   end
 
   def install_backup_gem
   	topic "Installing Backup Gem"
-  	comand "gem install backup"
+  	run_stdout "gem install backup"
   end
 
   def install_postgresql_client
   	topic "Installing Posgtgresql Client 9.3"
-  	command "wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -"
-	command "sudo apt-get update"
-	command "sudo apt-get install postgresql-client-9.3"
+  	run_stdout "wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -"
+	run_stdout "sudo apt-get update"
+	run_stdout "sudo apt-get install postgresql-client-9.3"
   end
 end
